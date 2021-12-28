1958a38a7d700e49b45c0e937d365cf5cee15bdd
Marc Schmidt <mrc.schmidt@gmx.net>
2021-12-28 21:34:52 +0100

    Some more Option funcs

--- option/option.go
+++ option/option.go
@@ -10,13 +10,29 @@ func Of[T any](value T) *Option[T] {
 	}
 }
 
+func Empty[T any]() *Option[T] {
+	return &Option[T]{
+		nil,
+	}
+}
+
+func (opt *Option[T]) IsPresent() bool {
+	return !opt.isNil()
+}
+
+func (opt *Option[T]) IfPresent(method func(value T)) {
+	if !opt.isNil() {
+		method(*opt.value)
+	}
+}
+
 // Go doesn't allow functions with receivers to declare type parameters, thus we can't map T to U
 // (and I refuse to pass Option as a parameter because it's ugly/inconsistent)
-func (opt *Option[T]) Map(mapFunc func(value T) T) *Option[T] {
+func (opt *Option[T]) Map(method func(value T) T) *Option[T] {
 	if opt.isNil() {
 		return opt
 	}
-	newValue := mapFunc(*opt.value)
+	newValue := method(*opt.value)
 	return &Option[T]{
 		&newValue,
 	}
@@ -31,6 +47,15 @@ func (opt *Option[T]) OrElse(alternativeValue T) T {
 		return alternativeValue
 	}
 	return *opt.value
+}
+
+func (opt *Option[T]) Or(alternativeValue T) *Option[T] {
+	if opt.isNil() {
+		return &Option[T]{
+			&alternativeValue,
+		}
+	}
+	return opt
 }
 
 func (opt *Option[T]) OrElseGet(alternativeOption *Option[T]) T {
