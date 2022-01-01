12a1fe7eb60b737d50b015fde3e65a1c8346ddb1
Marc Schmidt <mrc.schmidt@gmx.net>
2022-01-01 17:02:49 +0100

    Minor improvements

--- stream/collector.go
+++ stream/collector.go
@@ -1,11 +1,5 @@
 package stream
 
-import (
-	"sync"
-)
-
-var wg sync.WaitGroup
-
 type SliceCollector[T any] func(stream stream[T]) []T
 
 func (s *stream[T]) Collect(collector SliceCollector[T]) []T {
@@ -15,38 +9,34 @@ func (s *stream[T]) Collect(collector SliceCollector[T]) []T {
 
 func ToSlice[T any]() SliceCollector[T] {
 	return func(stream stream[T]) []T {
-		return applySequence(&stream.elements, &stream.sequence)
-		// wg.Add(len(stream.elements))
-		// for _, el := range stream.elements {
-		// 	go applyActions(&el, stream.sequence)
-		// }
-		// wg.Wait()
+		applySequence(&stream.elements, &stream.sequence)
+		return stream.elements
 	}
 }
 
-func applySequence[T any](elements *[]T, sequence *[]action[T]) []T {
+func applySequence[T any](elements *[]T, sequence *[]action[T]) {
 	for _, action := range *sequence {
 		switch action.actionType {
 		case filter:
-			*elements = applyFilter(elements, action.filterFunc)
+			applyFilter(elements, action.filterFunc)
 		}
 	}
-	return *elements
 }
 
-func applyFilter[T any](elements *[]T, filterFunc func(el T) bool) []T {
+func applyFilter[T any](elements *[]T, filterFunc func(el T) bool) {
 	var toRemove []int
 	for i, el := range *elements {
 		if !filterFunc(el) {
 			toRemove = append(toRemove, i)
 		}
 	}
-	return removeFromSlice(*elements, toRemove)
+	*elements = removeFromSlice(*elements, toRemove)
 }
 
-func removeFromSlice[T any](slice []T, remove []int) []T { // 2, 5, 13
+func removeFromSlice[T any](slice []T, remove []int) []T {
+	lenSlice, lenRemove := len(slice), len(remove)
 	for i := len(remove) - 1; i >= 0; i-- {
-		slice[remove[i]] = slice[len(slice)-(len(remove)-i)]
+		slice[remove[i]] = slice[lenSlice-(lenRemove-i)]
 	}
 	return slice[:len(slice)-len(remove)]
 }
