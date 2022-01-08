9205ab121755af41f1be7cc6c69754d4c55a3dab
Marc Schmidt <mrc.schmidt@gmx.net>
2022-01-08 17:32:19 +0100

    Add sort by length helper funcs

--- stream/sort/sort.go
+++ stream/sort/sort.go
@@ -13,3 +13,18 @@ func Ascending[T constraints.Ordered](element1 T, element2 T) bool {
 func Descending[T constraints.Ordered](element1 T, element2 T) bool {
 	return element1 < element2
 }
+
+func LenAscending[T lengthable](element1 T, element2 T) bool {
+	return len(element1) > len(element2)
+}
+
+func LenDescending[T lengthable](element1 T, element2 T) bool {
+	return len(element1) < len(element2)
+}
+
+type lengthable interface {
+	~string |
+		~[]any | ~[]int | ~[]string | ~[]bool | ~[]float32 | ~[]int64 | ~[]int32 | ~[]int16 | ~[]int8 | ~[]byte |
+		~map[any]any | ~map[string]any | ~map[string]int | ~map[string]bool | ~map[int]any | ~map[int]string | ~map[int]bool |
+		~chan any | ~chan string | ~chan int | ~chan bool | ~chan float32
+}
